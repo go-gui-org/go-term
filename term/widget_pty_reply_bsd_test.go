296a4bb8ce460d50452e8dd644b8bfb1e2fc7ae3
mike-ward <mike@mike-ward.net>
2026-07-09 13:58:15 -0500

    term: split pty reply test into platform-specific setRawMode files
    TIOCGETA/TIOCSETA are BSD-specific; TCGETS/TCSETS are used on Linux.
    Platform-agnostic test logic stays in widget_pty_reply_test.go with
    //go:build !windows.

---
+++ term/widget_pty_reply_bsd_test.go
@@ -0,0 +1,23 @@
+//go:build darwin || freebsd || netbsd || openbsd || dragonfly
+
+package term
+
+import "golang.org/x/sys/unix"
+
+// setRawMode puts a tty fd into raw mode so query replies (which carry no
+// newline) are delivered to Read immediately, as readline/ucs-detect do.
+func setRawMode(fd int) error {
+	t, err := unix.IoctlGetTermios(fd, unix.TIOCGETA)
+	if err != nil {
+		return err
+	}
+	raw := *t
+	raw.Iflag &^= unix.IGNBRK | unix.BRKINT | unix.PARMRK | unix.ISTRIP |
+		unix.INLCR | unix.IGNCR | unix.ICRNL | unix.IXON
+	raw.Lflag &^= unix.ECHO | unix.ECHONL | unix.ICANON | unix.ISIG | unix.IEXTEN
+	raw.Cflag &^= unix.CSIZE | unix.PARENB
+	raw.Cflag |= unix.CS8
+	raw.Cc[unix.VMIN] = 1
+	raw.Cc[unix.VTIME] = 0
+	return unix.IoctlSetTermios(fd, unix.TIOCSETA, &raw)
+}
